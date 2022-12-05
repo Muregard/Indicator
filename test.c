618f7c15ec623a336ebe6b451b905679fb2baac3
Muregard <117261085+Muregard@users.noreply.github.com>
2022-12-05 15:30:36 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -33,13 +33,106 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	//1.当不知道p应该初始化为什么地址的时候，直接初始化为NULL
+//	int* p = NULL;
+//
+//	//2.明确知道初始化的值
+//	int a = 10;
+//	int* ptr = &a;
+//	return 0;
+//}
+
+//#define N_VALUES 5
+//int main()
+//{
+//	float values[N_VALUES];
+//	float* vp;
+//	//指针+-整数；指针的关系运算
+//	for (vp = &values[0];vp < &values[N_VALUES];)
+//	{
+//		*vp++ = 0;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;//指向第一个元素
+//	int* pend = arr + 9;//指向最后一个元素
+//	while (p <= pend)
+//	{
+//		printf("%d\n", *p);
+//		p++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	char c[5];
+//	//指针和指针相减的前提：两个指针指向同一块空间
+//	printf("%d\n", &arr[9] - &c[0]);//err
+//	printf("%d\n", &arr[9] - &arr[0]);//指针-指针，得到中间包含的元素个数
+//	return 0;
+//}
+
+//1.库函数方法 - 求字符串长度
+//#include<string.h>
+
+//2.计数器方法 - 求字符串长度
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//3.递归方法 - 求字符串长度
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + my_strlen(str + 1);
+//	}
+//	else						 
+//		return 0;
+//}
+
+//4.指针-指针 - 求字符串长度	
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	while (*str != '\0')
+//	{
+//		str++;
+//	}
+//	return str - start;//指针-指针，得到中间元素个数
+//}
+//int main()
+//{
+//	//strlen(); - 求字符串长度
+//	//递归
+//	//int len = strlen("abc");
+//	int len = my_strlen("abc");
+//	printf("%d\n", len);
+//
+//	return 0;
+//}
+
+//指针数组 - 数组
+
 int main()
 {
-	//1.当不知道p应该初始化为什么地址的时候，直接初始化为NULL
-	int* p = NULL;
-
-	//2.明确知道初始化的值
-	int a = 10;
-	int* ptr = &a;
+	int arr[10];//整形数组 - 存放的是整形
+	char ch[5];//字符数组 - 存放的是字符
+	int* parr[5];//指针数组 - 存放的是指针
 	return 0;
 }
